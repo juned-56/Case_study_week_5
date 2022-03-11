be1e37eccb98f0708e7b6db6e217b31b03e352c5
Juned Khan <73770571+juned-56@users.noreply.github.com>
2022-03-11 16:48:17 +0530

    Update PizzaService.java

--- PizzaHouseStoreManagementApplication/src/com/pizzamangement/service/PizzaService.java
+++ PizzaHouseStoreManagementApplication/src/com/pizzamangement/service/PizzaService.java
@@ -4,8 +4,12 @@ import com.pizzamanagement.model.Pizza;
 
 public interface PizzaService {
 	//creating the pizza methods or services
+	//add pizza
 	public void addPizza(Pizza pizza);
+	//update pizza
 	public void updatePizza(Pizza pizza);
+	//delete pizza
 	public void deletePizza(Pizza pizza);
+	//find pizaa
 	Pizza findPizzaByNo(int pizzaNo);
 }
